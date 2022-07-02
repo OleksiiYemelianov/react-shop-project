fb970b5f225b6ce2ba01f0a3060ce56b756f2107
Oleksii <nebezotveta@gmail.com>
2022-07-02 16:59:33 +0300

    repo link added into the header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 function Footer() {
-    return         <footer className="page-footer #64dd17 light-green accent-4">
-    <div className="footer-copyright">
-      <div className="container">
-      © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+  return (
+    <footer className="page-footer #64dd17 light-green accent-4">
+      <div className="footer-copyright">
+        <div className="container">
+          © {new Date().getFullYear()} Copyright Text
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/OleksiiYemelianov/react-shop-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Repo
+          </a>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  );
 }
 
-export {Footer}
+export { Footer };
