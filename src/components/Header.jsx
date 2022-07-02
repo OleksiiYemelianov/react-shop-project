ab3b485bd1f681cb0fc3e14c6befca32ec598628
Oleksii <nebezotveta@gmail.com>
2022-07-02 17:19:16 +0300

    Header home link added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return   <nav className="#64dd17 light-green accent-4">
     <div className="nav-wrapper">
-      <a href="/" className="brand-logo">React Shop</a>
+      <a href="https://oleksiiyemelianov.github.io/react-shop-project/" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="https://github.com/OleksiiYemelianov/react-shop-project" target="_blank" rel="noreferrer">Repo</a></li>
       </ul>
